5cb1bde4296136c9ec63995f412f61949652359c
MacBook <macbook@MacBookdeMacBook.local>
2022-11-11 01:41:38 -0500

    a8 6.1

--- src/tuiter/tuits/tuits-list.js
+++ src/tuiter/tuits/tuits-list.js
@@ -1,12 +1,17 @@
-import React from "react";
-import {useSelector} from "react-redux";
+import React, {useEffect} from "react";
+import {useDispatch, useSelector} from "react-redux";
 import TuitsItem from "./tuits-item";
+import {findTuitsThunk} from "../../services/tuits-thunks";
 
 const TuitsList = () => {
-    const postsArray = useSelector(state => state.tuits)
+    const {tuits, loading} = useSelector(state => state.tuitsData)
+    const dispatch = useDispatch();
+    useEffect(() => {dispatch(findTuitsThunk())}, [])
+
     return(
         <ul className="list-group">
-            {postsArray.map(tuit => <TuitsItem tuit={tuit}/> )}
+            {loading && <li className="list-group-item">Loading...</li>}
+            {tuits.map(tuit => <TuitsItem tuit={tuit}/> )}
         </ul>
     );
 };
--- src/tuiter/tuits/tuits-stats.js
+++ src/tuiter/tuits/tuits-stats.js
@@ -5,11 +5,11 @@ const TuitsStats = ({tuit}) => {
         <div className="row mt-2">
             <div className="col-3">
                 <i className="bi bi-chat text-secondary"></i>
-                <span className="text-secondary"> {tuit.comments}</span>
+                <span className="text-secondary"> {tuit.replies}</span>
             </div>
             <div className="col-3">
                 <i className="bi bi-repeat text-secondary"></i>
-                <span className="text-secondary"> {tuit.retuit}</span>
+                <span className="text-secondary"> {tuit.retuits}</span>
             </div>
             <div className="col-3">
                 {tuit.liked? <i className="bi bi-heart-fill text-danger"></i> : <i className="bi bi-heart text-secondary"></i>}
